ff60b86d3c7bc724d5e39646964e2f067e5a1955
Brittani Baker <brittanibaker07@gmail.com>
2020-10-30 22:01:11 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+ def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push(next_color)
+  end
+  
+  def using_unshift(colors_in_the_rainbow, next_color)
+    colors_in_the_rainbow.unshift(next_color)
+  end
+  
+  def using_pop(continents)
+    continents = "Antarctica"
+    
+  end
+
+def pop_with_args(continents)
+  continents.pop(continents.size)
+  end
